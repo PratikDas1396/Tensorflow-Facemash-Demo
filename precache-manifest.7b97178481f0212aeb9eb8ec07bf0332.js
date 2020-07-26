31fdf528232df8c4f51c3962d7f70b046ce77cce
Pratik Das <pratikdas1396@gmail.com>
2020-07-26 16:50:11 +0530

    Updates

---
+++ precache-manifest.7b97178481f0212aeb9eb8ec07bf0332.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87248541089e0a74b4db631dd849bb61",
+    "url": "/Tensorflow-Facemash-Demo/index.html"
+  },
+  {
+    "revision": "5651369d79ca333d0516",
+    "url": "/Tensorflow-Facemash-Demo/static/css/main.62af4a8d.chunk.css"
+  },
+  {
+    "revision": "5ce245d2a8e194814097",
+    "url": "/Tensorflow-Facemash-Demo/static/js/2.12055793.chunk.js"
+  },
+  {
+    "revision": "9f3ece4be77bc30b04966fe84f4cf2e7",
+    "url": "/Tensorflow-Facemash-Demo/static/js/2.12055793.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5651369d79ca333d0516",
+    "url": "/Tensorflow-Facemash-Demo/static/js/main.901a7875.chunk.js"
+  },
+  {
+    "revision": "52787fa0fd2ed9fe2437",
+    "url": "/Tensorflow-Facemash-Demo/static/js/runtime-main.094a6343.js"
+  }
+]);
